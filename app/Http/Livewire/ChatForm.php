f069ecba432f9bb6506815f20013457ec4b0779c
ocampojosue <ocampojosue08@gmail.com>
2022-04-24 00:10:48 -0400

    Eventos entre vista y componente

--- app/Http/Livewire/ChatForm.php
+++ app/Http/Livewire/ChatForm.php
@@ -6,14 +6,18 @@ use Livewire\Component;
 
 class ChatForm extends Component
 {
-    public $nombre;
+    public $nombre, $mensaje;
 
     public function mount(){
         $this->nombre = "";
+        $this->mensaje = "";
     }
     
     public function render()
     {
         return view('livewire.chat-form');
+    }
+    public function enviarMensaje(){
+        $this->emit('mensajeEnviado');
     }
 }
--- resources/views/livewire/chat-form.blade.php
+++ resources/views/livewire/chat-form.blade.php
@@ -4,4 +4,28 @@
       <input type="text" name="" id="" class="form-control" wire:model="nombre" placeholder="" aria-describedby="helpId">
       <small class="">{{$nombre}}</small>
     </div>
+    <div class="form-group">
+      <label for="">Mensaje: </label>
+      <input type="text" name="" id="" class="form-control" wire:model="mensaje" placeholder="" aria-describedby="helpId">
+      <small class="">{{$mensaje}}</small>
+    </div>
+    <button type="button" class="btn btn-success" wire:click="enviarMensaje">Enviar</button>
+
+    {{-- Mensaje de Alerta --}}
+    <div style="position: relative" class="alert alert-success collapse" role="alert" id="avisoSuccess">
+      <script>
+        // Recibir en JS el emit desde el componente
+        // Evento mensajeEnviado
+        window.livewire.on('mensajeEnviado', function(){
+          // Mostra el aviso
+          $('#avisoSuccess').fadeIn('slow');
+          setTimeout(
+            function(){
+              $('#avisoSuccess').fadeOut('slow');
+            },
+            3000
+          );
+        });
+      </script>
+    </div>
 </div>
